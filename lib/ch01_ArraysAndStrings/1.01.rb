75f0f3be88fce36cc94af0c765ec14b922f78301
Mina Habib <minahabi@amazon.com>
2017-03-14 21:56:53 +0000

    Add another solution to 1.01

--- lib/ch01_ArraysAndStrings/1.01.rb
+++ lib/ch01_ArraysAndStrings/1.01.rb
@@ -1,7 +1,19 @@
 # page 90
 # Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
+# Solution 1
+
 def is_unique?(str)
+  str.length.times do |i|
+    return false if str.count(str[i]) > 1
+  end
+  return true
+end
+
+
+# Solution 2
+
+def is_unique2?(str)
   char_set = Hash.new()
   str.each_char do |i|
     return false if char_set[i] == true
